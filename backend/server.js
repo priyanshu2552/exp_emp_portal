4e257df655a8e3ad9ad58305c8bd84b67114e910
priyanshu2552 <priyanshuraj2552@gmail.com>
2025-05-21 18:08:40 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express=require('express')
 const app=express()
+require('dotenv').config();
+const connectDB = require('./config/db'); 
+connectDB()
 const port=5000
 app.get('/',(req,res)=>{
     res.send('hello world')
